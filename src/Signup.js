58b491f41b62edb7a2665973604e46b7218cca53
kathojumanoj <139505502+kathojumanoj@users.noreply.github.com>
2024-12-18 14:24:09 +0530

    Update Signup.js

--- src/Signup.js
+++ src/Signup.js
@@ -36,7 +36,7 @@ const Signup = () => {
       return;
     }
     try {
-      const response = await axios.post('http://localhost:3001/signup', {
+      const response = await axios.post('https://flight-booking-backend-omega.vercel.app/signup', {
         username,
         email,
         phno,
